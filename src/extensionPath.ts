71dc04286dcaee7e3024fb69e30745e18478944f
Bill Tsui <b10902118@ntu.edu.tw>
2025-11-08 02:13:35 +0800

    fix: not init extensionPath for browser env

--- src/extensionPath.ts
+++ src/extensionPath.ts
@@ -1,10 +1,14 @@
-import { fileURLToPath } from "url";
-import path from "path";
+let CHROME_INSPECTOR_SYNC_EXTENSION_PATH: string | undefined;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+if (typeof window === "undefined") {
+  const u = "url";
+  const { fileURLToPath } = await import(u);
+  const p = "path";
+  const path = await import(p);
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
 
-export const CHROME_INSPECTOR_SYNC_EXTENSION_PATH = path.join(
-  __dirname,
-  "extension",
-);
+  CHROME_INSPECTOR_SYNC_EXTENSION_PATH = path.join(__dirname, "extension");
+}
+
+export { CHROME_INSPECTOR_SYNC_EXTENSION_PATH };
